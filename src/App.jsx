71082df2748a5ce082247c2df155be6de88f62a4
jorjeGs <jgs_23072000@outlook.com>
2023-12-22 21:05:15 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='App'>
+      <h1>ToDo MetriXs</h1>
+      <div className='container'>
+        <h2>Welcome!</h2>
+        <p>This is just the begining, let's grow <strong>together</strong></p>
+      </div>
+    </main>
+  )
+}
+
+export default App
