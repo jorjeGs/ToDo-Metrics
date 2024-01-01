cfe9a8f6e38cde7d12a5628ebc71b14fef93a923
jorjeGs <jgs_23072000@outlook.com>
2023-12-31 20:22:44 -0800

    minor overflow bug fix

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -13,7 +13,7 @@ const TaskCard = (props) => {
 
     return (
         <>
-            <div className="task flex flex-col mx-auto justify-center items-center overflow-hidden w-full bg-white shadow-xl border-2 border-gray-200 rounded-md p-1 transition duration-200 transform hover:scale-110">
+            <div className="task flex flex-col mx-auto justify-center items-center overflow-hidden w-full sm:w-11/12 bg-white shadow-xl border-2 border-gray-200 rounded-md p-1 transition duration-200 transform hover:scale-110">
                 <button className="w-full h-full" onClick={handleModal}>
                     <h3 className="sm:text-xl text-xs font-bold text-gray-800 overflow-hidden whitespace-nowrap text-ellipsis">{title}</h3>
                     <small className="text-gray-400 text-center my-auto sm:text-base text-xs overflow-hidden">{date}</small>
