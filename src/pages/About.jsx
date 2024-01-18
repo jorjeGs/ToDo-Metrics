b5dfa616238f8e4bca38bcc8594903765ca2c68a
jorjeGs <jgs_23072000@outlook.com>
2024-01-17 22:05:40 -0800

    about and notFound page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,27 +4,18 @@ import WelcomeNavbar from "../components/WelcomeNavbar"
 
 const About = () => {
 
-    const images = [
-        '/html.png',
-        '/css.png',
-        '/js.png',
-        '/react.png',
-        '/tailwind.png',
-        '/github.png',
-    ]
-
     return (
         <>
             <header>
                 <WelcomeNavbar />
             </header>
             <div className='h-screen'>
-                <div id='first' className="relative bg-white h-1/2">
+                <div id='first' className="relative bg-inherit h-1/2">
                     <h2 className='text-xl text-gray-400 text-center'>About</h2>
                     <div className='flex flex-col-reverse md:flex-row justify-center sm:gap-0 gap-3 p-2'>
                         <div className='flex flex-col md:w-1/2 justify-center text-center h-auto mt-3 gap-2'>
-                            <h1 className='text-6xl text-black font-bold'>Hi!</h1>
-                            <h2 className='sm:text-2xl text-lg text-balance text-center text-black'>This app was developed as a portfolio project for my web developer resume.</h2>
+                            <h1 className='text-6xl text-black dark:text-neutral-100 font-bold'>Hi!</h1>
+                            <h2 className='sm:text-2xl text-lg text-balance text-center text-black dark:text-neutral-300'>This app was developed as a portfolio project for my web developer resume.</h2>
                         </div>
                         <div className='flex flex-col md:w-1/2 justify-end h-full items-center'>
                             <img className='md:w-1/2 w-1/3 mx-auto min-w-40 max-w-52 md:max-w-72 rounded-full border-8 border-gray-300 transform transition duration-300 hover:scale-110' src='/user.png' alt='Jorge Garcia' />
@@ -32,17 +23,18 @@ const About = () => {
                     </div>
                     <div className="custom-shape-divider-bottom-1703831514 pointer-events-none">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                            <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="fill-gray-200"></path>
+                            <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="fill-gray-200 dark:fill-neutral-600"></path>
                         </svg>
                     </div>
                 </div>
-                <div className="relative bg-gray-200 h-auto min-h-1/2 p-3">
+                <div className="relative bg-gray-200 dark:bg-neutral-600 h-auto min-h-1/2 p-3">
                     <div className='flex flex-col justify-center items-center gap-6'>
                         <div className='flex flex-col justify-center items-center text-center mt-4 md:gap-6 gap-4'>
                             <img className='md:w-1/6 w-3/12 mx-auto' src='/todo-logo.svg' alt='Taskflow' />
-                            <h1 className='text-4xl text-black font-bold'>Taskflow</h1>
-                            <h2 className='sm:text-xl text-md text-balance text-black'><strong className='text-blue-600'>Taskflow</strong> was made thinking as a Trello-based app with <strong>no registered account need</strong>.</h2>
-                            <h2 className='sm:text-xl text-md text-balance text-black'>Organize your daily tasks with your progress <strong>locally storage</strong> in your browser.</h2>
+
+                            <h1 className='text-4xl text-black dark:text-neutral-100 font-bold'>Taskflow</h1>
+                            <h2 className='sm:text-xl text-md text-balance text-black dark:text-neutral-300'><strong className='text-blue-600 dark:text-neutral-100'>Taskflow</strong> was made thinking as a Trello-based app with <strong>no registered account need</strong>.</h2>
+                            <h2 className='sm:text-xl text-md text-balance text-black dark:text-neutral-100'>Organize your daily tasks with your progress <strong>locally storage</strong> in your browser.</h2>
                         </div>
                         <div className='flex flex-wrap justify-center items-center gap-6 mb-9'>
                             <img className='md:max-w-24 max-w-12' src='/html.png' alt='HTML' />
@@ -54,7 +46,7 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <div className="relative bg-[#004B87] h-auto">
+                <div className="relative bg-[#004B87] dark:bg-neutral-500 h-auto">
                     <div className='flex flex-col md:flex-row justify-center items-center gap-6 p-2'>
                         <div className='flex flex-col justify-center items-center text-wrap text-center mt-4 gap-6 mb-6'>
                             <p className='text-2xl text-white'>Currently looking for job!</p>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import WelcomeNavbar from '../components/WelcomeNavbar';
 
 const NotFound = () => {
     return (
+        <>
+        <header>
+            <WelcomeNavbar />
+        </header>
         <div className="flex flex-col w-full mx-auto my-auto justify-center items-center h-full">
-            <h1 className="text-3xl">404</h1>
-            <h2 className="">Page Not Found</h2>
+            <h1 className="text-3xl dark:text-neutral-300">404</h1>
+            <h2 className="dark:text-neutral-400">Page Not Found</h2>
         </div>
+        </>
     );
 }
 
